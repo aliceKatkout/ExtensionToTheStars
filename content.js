0c014543b8f40c5e0b28e3dc2c57b0c0b956251c
Alice <72550777+aliceKatkout@users.noreply.github.com>
2020-10-26 14:45:05 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,12 @@
-const TitleType = document.getElementsByTagName("h1");
-const ArrayOfTitle = Array.from(TitleType);
+const TitleType = document.getElementsByTagName("h1"); //recupere les h1
+const ArrayOfTitle = Array.from(TitleType); // Créé un tableau avec les h1
 
-function updateFontColor(el) {
+function updateFontColor(el) { 
   el.style.color = "green";
-}
+} 
+// change la couleur des h1
 
-ArrayOfTitle.forEach(updateFontColor);
+ArrayOfTitle.forEach(updateFontColor); //applique la fonction sur tous les titres du tableau
 
 
 function updateSourceImage() {
@@ -13,20 +14,22 @@ function updateSourceImage() {
    const ArrayOfImages = Array.from(ImageType);
    ArrayOfImages.forEach(updateSameImage);
  }
-
+//récupère les images, créé un tableau, et applique la fonction updateSameImage sur chaque élément du tableau
 
 function updateSameImage(el) {
   chrome.storage.sync.get(
     'monImage',
     function(result) {
-      // @see https://subscription.packtpub.com/book/web_development/9781783287314/1/ch01lvl1sec10/the-callback-pattern
       el.src = result.monImage;
     }
   );
  }
+// Récupère l'url stockée dans google storage par l'utilisateur via la fenêtre popup, marche grâce à une fonction call back
+// @see https://subscription.packtpub.com/book/web_development/9781783287314/1/ch01lvl1sec10/the-callback-pattern
 
-updateSourceImage();
+updateSourceImage(); // Applique la fonction
 
+// Tout ce qui suit ne sont que des tests n'y prêter pas attention
 // const ImageType = document.getElementsByTagName("img");
 // const ArrayOfImages = Array.from(ImageType);
 //
@@ -35,3 +38,4 @@ updateSourceImage();
 // }
 //
 // ArrayOfImages.forEach(updateSameImage);
+
